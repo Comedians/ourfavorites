1865b27d3e77e93f065e70b1a92d8e234e55c309
Estrada303 <eestra13@msudenver.edu>
2020-02-18 14:55:49 -0700

    First commit

---
+++ Hungry.java
@@ -0,0 +1,10 @@
+public class Hungry{
+
+	public static void main (String[] args){
+
+		System.out.println("What is you favorite food?");
+		//answer below
+		System.out.println("Ernie: I like tacos");
+		System.out.println("Uriel: I like enchiladas");
+	}//main
+}//class hungry
